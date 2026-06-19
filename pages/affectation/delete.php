62a286a7fde3914a5bab764fc0d948d24bb0099f
Mouad-Hassan <mouad6663@gmail.com>
2026-06-19 15:41:10 +0100

    terminer le contenu de page matiere

--- pages/affectation/delete.php
+++ pages/affectation/delete.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-require_once DIR . '/../../config/database.php';
+require_once __DIR__ . '/../../config/database.php';
 
 $pdo = getDBConnection();
 $id  = (int)($_GET['id'] ?? 0);
